1c0e79440bd128a6ca63378d370a80ffde5d92f0
Olegshkradiuk <Olegwills40@gmail.com>
2025-09-20 12:58:20 +0300

    hw1

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 
 def bSearchSortedList(lst: list[int], num: int) -> int:
-    # takes sorted list and a number
-    # returns index of the number if exists, otherwise -1
+
     left: int = 0
     right: int = len(lst) - 1
-    middle: int = (left + right) // 2
-    while left <= right and lst[middle] != num:
-        if num < lst[middle]:
+    defp: int = len(lst)
+
+    while left <= right:
+        middle: int = (left + right) // 2
+        if num <= lst[middle]:
+            defp = middle
             right = middle - 1
         else:
             left = middle + 1
-        middle = (right + left) // 2
-    return middle if left <= right else -1
+    if defp < len(lst) and lst[defp] == num:
+        return defp
+    else:
+        return -(defp) - 1
 
 
-numbers: list[int] = [1, 5, 20, 20, 20, 20, 20, 30, 100]
-print(bSearchSortedList(numbers, 4))
+numbers1: list[int] = [1, 5, 20, 20, 20, 20, 20, 30, 100]
+print(bSearchSortedList(numbers1, 4))  # -2
+
+numbers2: list[int] = [20, 20, 20, 20, 20, 20]
+print(bSearchSortedList(numbers2, 20))  # 0
+
+numbers3: list[int] = [20, 30, 40, 50, 60]
+print(bSearchSortedList(numbers3, 45))  # -4
