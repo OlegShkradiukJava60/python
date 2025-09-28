ca6218840e753f21c82f58bc64a2ab1b822ec6bc
Olegshkradiuk <Olegwills40@gmail.com>
2025-09-28 19:21:25 +0300

    hw22 sol yuri

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 
-def bSearchSortedList(lst: list[int], num: int) -> int:
-    # takes sorted list and a number
-    # returns index of the number if exists, otherwise -1
-    left: int = 0
-    right: int = len(lst) - 1
-    middle: int = (left + right) // 2
-    while left <= right and lst[middle] != num:
-        if num < lst[middle]:
-            right = middle - 1
+def isSumTwo(numbers: list[int], sum: int):
+    helper: set[int] = set()
+    indx: int = 0
+    res: bool = False
+    length: int = len(numbers)
+    while indx < length and not res:
+        if sum - numbers[indx] in helper:
+            res = True
         else:
-            left = middle + 1
-        middle = (right + left) // 2
-    return middle if left <= right else -1
+            helper.add(numbers[indx])
+            indx += 1
+    return res
 
 
-numbers: list[int] = [1, 5, 20, 20, 20, 20, 20, 30, 100]
-print(bSearchSortedList(numbers, 4))
+def maxNegativeRepr(numbers: list[int]):
+    res: int = 0
+    helper: set[int] = set()
+    for num in numbers:
+        if abs(num) > res and -num in helper:
+            res = abs(num)
+        else:
+            helper.add(num)
+    return res if res else -1
+
+#  N ^ 2 implementation
+
+
+def find_sums(arr, target):
+    result = False
+    if not arr or len(arr) < 2:
+        return False
+    for i, num in enumerate(arr):
+        complement = target - num
+        # Проверяем что complement есть в оставшейся части массива
+        if complement in arr[i+1:]:
+            result = True
+            break
+
+    return result
+
+
+
+
