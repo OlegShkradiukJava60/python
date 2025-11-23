f575f6d1407d156906c077bfedd95c640f8aaa72
Olegshkradiuk <Olegwills40@gmail.com>
2025-11-23 11:43:30 +0200

    sol

--- regular_expressions.py
+++ regular_expressions.py
@@ -8,10 +8,10 @@ def passwordRe():
 
 def ipV4AddressRe() -> str:
     part = r"(25[0-5]|2[0-4]\d|1\d\d|0?\d?\d)"
-    return part + r"(\." + part + r"){3}"
+    return fr"{part}(\.{part}){{3}}"
 
 
 def mobileIsraelNumberRe() -> str:
     prefix = r"(?:\+972-5[0-9]-?|05[0-9]-?)"
     subscriber = r"(?:\d{7}|\d{3}-\d{2}-\d{2}|\d-\d{2}-\d{2}-\d{2})"
-    return prefix + subscriber
+    return fr"{prefix}{subscriber}"
